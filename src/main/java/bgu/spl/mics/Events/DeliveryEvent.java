9e91c0af1968e57bb45329998dec94a49ad9ef98
maor <maorrocky@gmail.com>
2018-12-08 21:13:29 +0200

    shabat

---
+++ src/main/java/bgu/spl/mics/Events/DeliveryEvent.java
@@ -0,0 +1,21 @@
+package bgu.spl.mics.Events;
+
+import bgu.spl.mics.Event;
+
+public class DeliveryEvent implements Event {
+    private int customerId;
+    private String address;
+
+    public DeliveryEvent(int customerId, String address) {
+        this.customerId = customerId;
+        this.address = address;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}

e3f625ec67adeb9a73adb56444b996292b41410e
maorrocky <maorrocky@gmail.com>
2018-12-09 16:47:59 +0200

    changed inventory

--- src/main/java/bgu/spl/mics/application/passiveObjects/ResourcesHolder.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/ResourcesHolder.java
@@ -14,46 +14,50 @@ import java.util.Collection;
  * You can add ONLY private methods and fields to this class.
  */
 public class ResourcesHolder {
-	private static class SingeltonHolder {
-		private static ResourcesHolder instance = new ResourcesHolder();
-	}
-	private Collection<DeliveryVehicle> vehicles;
-	
-	/**
+    private static class SingletonHolder {
+        private static ResourcesHolder instance = new ResourcesHolder();
+    }
+
+    private Collection<DeliveryVehicle> vehicles;
+
+    /**
      * Retrieves the single instance of this class.
      */
-	public static ResourcesHolder getInstance() {
-		return SingeltonHolder.instance;
-	}
-	
-	/**
+    public static ResourcesHolder getInstance() {
+        return SingletonHolder.instance;
+    }
+
+    /**
      * Tries to acquire a vehicle and gives a future object which will
      * resolve to a vehicle.
      * <p>
-     * @return 	{@link Future<DeliveryVehicle>} object which will resolve to a 
-     * 			{@link DeliveryVehicle} when completed.   
+     *
+     * @return {@link Future<DeliveryVehicle>} object which will resolve to a
+     * {@link DeliveryVehicle} when completed.
      */
-	public Future<DeliveryVehicle> acquireVehicle() {
-		return new Future<DeliveryVehicle>();
-	}
-	
-	/**
+    public Future<DeliveryVehicle> acquireVehicle() {
+        return new Future<DeliveryVehicle>();
+    }
+
+    /**
      * Releases a specified vehicle, opening it again for the possibility of
      * acquisition.
      * <p>
-     * @param vehicle	{@link DeliveryVehicle} to be released.
+     *
+     * @param vehicle {@link DeliveryVehicle} to be released.
      */
-	public void releaseVehicle(DeliveryVehicle vehicle) {
-		//TODO: Implement this
-	}
-	
-	/**
+    public void releaseVehicle(DeliveryVehicle vehicle) {
+        //TODO: Implement this
+    }
+
+    /**
      * Receives a collection of vehicles and stores them.
      * <p>
-     * @param vehicles	Array of {@link DeliveryVehicle} instances to store.
+     *
+     * @param vehicles Array of {@link DeliveryVehicle} instances to store.
      */
-	public void load(DeliveryVehicle[] vehicles) {
-		//TODO: Implement this
-	}
+    public void load(DeliveryVehicle[] vehicles) {
+        //TODO: Implement this
+    }
 
 }

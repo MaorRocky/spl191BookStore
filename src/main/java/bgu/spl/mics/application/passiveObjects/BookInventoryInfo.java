609c5a41048982c9c64ee0d12303e15b2baf5072
maorrocky <maorrocky@gmail.com>
2018-12-10 17:13:13 +0200

    changed inventory

--- src/main/java/bgu/spl/mics/application/passiveObjects/BookInventoryInfo.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/BookInventoryInfo.java
@@ -1,59 +1,69 @@
 package bgu.spl.mics.application.passiveObjects;
 
 
+import java.util.concurrent.Semaphore;
+
 /**
  * Passive data-object representing a information about a certain book in the inventory.
- * You must not alter any of the given public methods of this class. 
+ * You must not alter any of the given public methods of this class.
  * <p>
  * You may add fields and methods to this class as you see fit (including public methods).
  */
 public class BookInventoryInfo {
-	private String bookTitle;
-	private int amountInInventory;
-	private int price;
+    private Semaphore semaphore;
+    private String bookTitle;
+    private int amountInInventory;
+    private int price;
 
-	public BookInventoryInfo(String name, int amount, int price) {
-		bookTitle = name;
-		amountInInventory = amount;
-		this.price = price;
-	}
+    public BookInventoryInfo(String name, int amount, int price) {
+        bookTitle = name;
+        amountInInventory = amount;
+        this.price = price;
+        this.semaphore = new Semaphore(1);
+    }
 
-	/**
+    /**
      * Retrieves the title of this book.
      * <p>
-     * @return The title of this book.   
+     *
+     * @return The title of this book.
      */
-	public String getBookTitle() {
-		return bookTitle;
-	}
+    public String getBookTitle() {
+        return bookTitle;
+    }
 
-	/**
+    /**
      * Retrieves the amount of books of this type in the inventory.
      * <p>
-     * @return amount of available books.      
+     *
+     * @return amount of available books.
      */
-	public synchronized int getAmountInInventory() {
-		return amountInInventory;
-	}
+    public synchronized int getAmountInInventory() {
+        return amountInInventory;
+    }
 
-	/**
+    /**
      * Retrieves the price for  book.
      * <p>
+     *
      * @return the price of the book.
      */
-	public int getPrice() {
-		return price;
-	}
+    public int getPrice() {
+        return price;
+    }
 
-	public synchronized boolean reduce () {
-		if (getAmountInInventory() > 0) {
-			amountInInventory = amountInInventory - 1;
-			return true;
-		}
-		return false;
-	}
-	
-	
+    public boolean reduce() throws InterruptedException {
+        if (getAmountInInventory() > 0) {
+            semaphore.acquire();
+            try {
+                amountInInventory = amountInInventory - 1;
+                return true;
+            } finally {
+                semaphore.release();
+            }
+        }
+        return false;
+    }
 
-	
+
 }

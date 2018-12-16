6fe3e6cd90c2b6d0b8fac835cff6f037bce6bffa
maor rocky <maorrocky@gmail.com>
2018-12-16 18:10:31 +0200

    Merge branch 'master' of https://github.com/MaorRocky/spl191BookStore
    # Conflicts:
    #	src/main/java/bgu/spl/mics/application/passiveObjects/ResourcesHolder.java

---
+++ src/test/java/bgu/spl/mics/FutureTest.java
@@ -0,0 +1,17 @@
+package bgu.spl.mics;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class FutureTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
